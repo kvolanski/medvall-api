d6ed9471379ee89ad243c633577fcf4bd8ed58b2
Kevin Volanski <kvolanski@sdnadigital.com>
2024-06-21 09:49:02 -0300

    Create method detalhar and change method cadastrar.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
@@ -18,9 +20,11 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @PostMapping
-    public ResponseEntity cadastrar(@RequestBody @Valid CadastroMedicoDTO cadastroMedicoDTO){
-        medicoRepository.save(new Medico(cadastroMedicoDTO));
-        return ResponseEntity.ok().build();
+    public ResponseEntity cadastrar(@RequestBody @Valid CadastroMedicoDTO cadastroMedicoDTO, UriComponentsBuilder uriComponentsBuilder){
+        var medico = new Medico(cadastroMedicoDTO);
+        medicoRepository.save(medico);
+        var uri = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
@@ -43,6 +47,12 @@ public class MedicoController {
         var medico = medicoRepository.getReferenceById(id);
         medico.excluir();
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var medico = medicoRepository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
 }
