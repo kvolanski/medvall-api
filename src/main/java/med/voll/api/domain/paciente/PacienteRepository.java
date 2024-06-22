91a7ea984bbc84a97c58e9752e7b1b648ae302cc
Kevin Volanski <kvolanski@sdnadigital.com>
2024-06-21 23:26:32 -0300

    Fix errors

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,11 +1,7 @@
 package med.voll.api.domain.paciente;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
-    Optional<Object> findAllByAtivoTrue(Pageable pageable);
 }
